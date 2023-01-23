61166d1378cc968d1be6cc2551faac404a5c4930
Arban Hossain <arbanhossain@gmail.com>
2023-01-23 17:24:07 +0600

    bisection, false position

---
+++ NumericalMethods.py
@@ -0,0 +1,27 @@
+def bisection(f, a, b, tol=1e-5):
+  """Find root of f(x) = 0 on interval [a,b] to within tolerance tol using bisection method"""
+  if f(a)*f(b) > 0:
+    raise ValueError("f(a) and f(b) must have opposite signs")
+  while b - a > tol:
+    c = (a + b)/2
+    if f(c) == 0:
+      return c
+    elif f(a)*f(c) < 0:
+      b = c
+    else:
+      a = c
+  return (a + b)/2
+
+def false_position(f, a, b, tol=1e-5):
+  """Find root of f(x) = 0 on interval [a,b] to within tolerance tol using regular falsi method"""
+  if f(a)*f(b) > 0:
+    raise ValueError("f(a) and f(b) must have opposite signs")
+  while b - a > tol:
+    c = (a*f(b) - b*f(a))/(f(b) - f(a))
+    if f(c) == 0:
+      return c
+    elif f(a)*f(c) < 0:
+      b = c
+    else:
+      a = c
+  return (a + b)/2
